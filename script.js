c546ae9cacba910348a8e7ece557935e099fabf5
vivekkalal09 <vivekkalal09@gmail.com>
2025-08-20 12:37:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const button = document.querySelector('.btn');
+const url = "https://apps.midinnings.io/webhook-test/google-my-business-insights"
+
+const sendData = () => {
+    const locationId = document.querySelector('input').value
+    const value_location_id = JSON.stringify(locationId)
+
+    if (locationId.length === 19) {
+        const response = fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({ locationId })
+            body: value_location_id
+        })
+        button.innerText = "Connected";
+        button.style.backgroundColor = "green";
+        button.style.color = "white";
+        if (response) {
+            alert("Webhook triggered successfully");
+        } else {
+            alert("Failed to trigger webhook");
+        }
+    } else {
+        alert(`Invalid Location ID`);
+    }
+
+}
+
+
+button.addEventListener("click", sendData);
+
